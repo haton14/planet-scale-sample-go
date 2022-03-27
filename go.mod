eeaf2fa3fefa81d3d5a5ad290db73703eebf5c6f
haton14 <iioka.takumi.quena@gmail.com>
2022-03-27 18:34:23 +0900

    sample

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/haton14/planet-scale-sample-go
 
 go 1.18
+
+require (
+	github.com/go-sql-driver/mysql v1.6.0
+	github.com/joho/godotenv v1.4.0
+)
+
+require github.com/jmoiron/sqlx v1.3.4 // indirect
--- main.go
+++ main.go
@@ -1,5 +1,54 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
 
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	if err := godotenv.Load(".env"); err != nil {
+		panic(err)
+	}
+	db, err := connection()
+	defer db.Close()
+	if err != nil {
+		panic(err)
+	}
+	if err := db.Ping(); err != nil {
+		panic(err)
+	}
+	fmt.Println("Successfully connected to PlanetScale!")
+
+	users, err := readUsers(db)
+	if err != nil {
+		panic(err)
+	}
+	for _, u := range users {
+		fmt.Println(u.ID, ", ", u.Email, ", ", u.FistName, ", ", u.LastName)
+	}
+}
+
+func connection() (*sqlx.DB, error) {
+	db, err := sqlx.Open("mysql", os.Getenv("DSN"))
+	return db, err
+}
+
+type users struct {
+	ID       int    `db:"id"`
+	Email    string `db:"email"`
+	FistName string `db:"first_name"`
+	LastName string `db:"last_name"`
+}
+
+func readUsers(readDB *sqlx.DB) ([]users, error) {
+	users := []users{}
+	err := readDB.Select(&users, `select * from users`)
+	if err != nil {
+		return nil, err
+	}
+	return users, nil
 }
